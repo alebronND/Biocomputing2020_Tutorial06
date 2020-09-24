30716fda885827b88df82a542b5f75a4ee6baf44
grashoff <grashoff@nd.edu>
2020-09-24 19:52:48 -0400

    added the tr command to Q3 to clean up the data

--- step3.sh
+++ step3.sh
@@ -1,7 +1,6 @@
 # subtracts the minimum wages for those that had 12 years of school from the minimum wage of those that had 16 years of school (regardless of gender)
 # Usage: step3.sh (no input needed)
 
-hs=$(cat wages.csv | cut -d , -f 3,4 | grep "12," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
-col=$(cat wages.csv | cut -d , -f 3,4 | grep "16," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
-echo $col - $hs | bc
-
+hs=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep -w "12" | sort -k 2 -n | head -n 1 | cut -d " " -f 2)
+col=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep -w "16" | sort -k 2 -n | head -n 1 | cut -d " " -f 2)
+echo "$col-$hs" | bc
