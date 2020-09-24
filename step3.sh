e5a540141d887f30ed4ba15294f260c1c5904388
grashoff <grashoff@nd.edu>
2020-09-24 17:33:54 -0400

    addition of Question 3's answer script

---
+++ step3.sh
@@ -0,0 +1,7 @@
+# subtracts the minimum wages for those that had 12 years of school from the minimum wage of those that had 16 years of school (regardless of gender)
+# Usage: step3.sh (no input needed)
+
+hs=$(cat wages.csv | cut -d , -f 3,4 | grep "12," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
+col=$(cat wages.csv | cut -d , -f 3,4 | grep "16," | sort -t , -k 2 -n | head -n 1 | cut -d , -f 2)
+echo $hs
+
