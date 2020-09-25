dec7beedc1992ab375f116bc3eb92f89634e509f
grashoff <grashoff@nd.edu>
2020-09-24 20:02:29 -0400

    Added a sed command at the beginning to fix syntax

--- step3.sh
+++ step3.sh
@@ -1,6 +1,6 @@
 # subtracts the minimum wages for those that had 12 years of school from the minimum wage of those that had 16 years of school (regardless of gender)
 # Usage: step3.sh (no input needed)
 
-hs=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep -w "12" | sort -k 2 -n | head -n 1 | cut -d " " -f 2)
-col=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep -w "16" | sort -k 2 -n | head -n 1 | cut -d " " -f 2)
+hs=$(sed 's/\r$//' wages.csv | cut -d , -f 3,4 | tr "," " " | grep -w "12" | sort -k 2 -n | head -n 1 | cut -d " " -f 2)
+col=$(sed 's/\r$//' wages.csv | cut -d , -f 3,4 | tr "," " " | grep -w "16" | sort -k 2 -n | head -n 1 | cut -d " " -f 2)
 echo "$col-$hs" | bc
